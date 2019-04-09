229d051348bc2ff2b45ff0b37bbcdd1c226550ce
Guido van de Riet <guidovdriet@gmail.com>
2019-04-09 14:13:09 +0200

    Update second section

--- src/components/sections/Products.js
+++ src/components/sections/Products.js
@@ -6,39 +6,76 @@ import Header from "../layout/Header"
 import Section from "../layout/Section"
 
 // Images
-import image from "../../images/family-car.png"
+import familyCar from "../../images/family-car.png"
+import workGuy from "../../images/work-guy.png"
 
 const Products = () => {
   return (
-    <StyledProducts>
-      <Header>Products</Header>
-      <Intro>
-        Safe roads are the basis for a SafeCity. We use spacedata to warn,
-        inform and advise consumers as well as local authorities.
-      </Intro>
-      <Wrapper>
-        <Left>
-          An application made for consumers. In this sense of the word, road
-          users. In this concept we index Dutch roads and give them a safety
-          score. An example could be the difference in height, which might be a
-          puthole or wear and tear. Another index is the amount of lights near
-          the road. Road’s that aren’t well lit are often seen in the same areas
-          as traffic accidents - and even deaths. We hope to make drivers aware
-          of these roads and give them a safe trip to their destination. In the
-          future we might have travel advice based on other factors like the
-          weather.
-        </Left>
-        <Right>
-          <img src={image} />
-        </Right>
-      </Wrapper>
-    </StyledProducts>
+    <>
+      <StyledProducts>
+        <First>
+          <Header>Products</Header>
+          <Intro>
+            Safe roads are the basis for a SafeCity. We use spacedata to warn,
+            inform and advise consumers as well as local authorities.
+          </Intro>
+          <Wrapper>
+            <Left>
+              1. An application made for consumers. In this sense of the word,
+              road users. In this concept we index Dutch roads and give them a
+              safety score. An example could be the difference in height, which
+              might be a puthole or wear and tear. Another index is the amount
+              of lights near the road. Road’s that aren’t well lit are often
+              seen in the same areas as traffic accidents - and even deaths. We
+              hope to make drivers aware of these roads and give them a safe
+              trip to their destination. In the future we might have travel
+              advice based on other factors like the weather.
+            </Left>
+            <Right>
+              <img src={familyCar} alt="car" />
+            </Right>
+          </Wrapper>
+        </First>
+      </StyledProducts>
+      <Second>
+        <StyledProducts>
+          <Intro>
+            Safe roads are the basis for a SafeCity. We use spacedata to warn,
+            inform and advise consumers as well as local authorities.
+          </Intro>
+          <Wrapper>
+            <Left>
+              2. An application made for local authorities. Using the same data
+              but in a different way. This way the authorities can anticapate
+              problems on roads and other areas by checking the space data we
+              supply. This way we can highlight ‘problem’ areas. Which makes it
+              easier to send workers or other services to fix or prevent the
+              problems. By having an interactive map we make it easier for the
+              authorities to make decisions based on the gravity of an area. In
+              the future, drivers might be able to vote on particular areas to
+              raise concern. This way the community has a vote and you have
+              input from different sources.
+            </Left>
+            <Right>
+              <img src={workGuy} alt="car" />
+            </Right>
+          </Wrapper>
+        </StyledProducts>
+      </Second>
+    </>
   )
 }
 
 export default Products
 
 const StyledProducts = styled(Section)``
+
+const First = styled.div``
+
+const Second = styled.div`
+  background-color: black;
+  color: white;
+`
 
 const Intro = styled.div`
   width: 40%;
@@ -62,4 +99,7 @@ const Right = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  img {
+    width: 350px;
+  }
 `
